0f5d35dcf5a74debb34124be9a41f916bd9df6fb
rahulsai1999 <noneuser2183@gmail.com>
2019-12-25 12:49:26 +0530

    initial commit

---
+++ src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	example "./lib"
+	"github.com/gin-gonic/gin"
+)
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func postform(c *gin.Context) {
+	name := c.PostForm("name")
+	message := c.PostForm("message")
+	c.JSON(200, gin.H{
+		"name":    name,
+		"message": message,
+	})
+}
+
+func main() {
+	fmt.Println("Hello")
+	router := gin.Default()
+
+	// route handling with anonymous functions
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello",
+		})
+	})
+
+	//route handling with named functions
+	router.GET("/ping", ping)
+
+	//route handling with refactoring
+	router.GET("/external", example.External)
+
+	//post routes
+	router.POST("/normal", postform)
+
+	router.Run("0.0.0.0:5000")
+}
