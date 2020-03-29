73811afed98cc18f7bb5e09b9dcea630910af7ff
rahulsai1999 <noneuser2183@gmail.com>
2020-03-29 22:57:32 +0530

    added go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rahulsai1999/go-rest
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.2
--- main.go
+++ main.go
@@ -1,47 +1,25 @@
 package main
 
-import (
-	"fmt"
+import "github.com/gin-gonic/gin"
 
-	controller "github.com/go-rest/controllers"
-
-	"github.com/gin-gonic/gin"
-)
-
-func ping(c *gin.Context) {
-	c.JSON(200, gin.H{
+func ping(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
 		"message": "pong",
 	})
 }
 
-func postform(c *gin.Context) {
-	name := c.PostForm("name")
-	message := c.PostForm("message")
-	c.JSON(200, gin.H{
-		"name":    name,
-		"message": message,
-	})
-}
-
 func main() {
-	fmt.Println("Hello")
 	router := gin.Default()
 
-	// route handling with anonymous functions
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello",
+	// route handling basic
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Welcome to API",
 		})
 	})
 
-	//route handling with named functions
+	// route handling for named functions
 	router.GET("/ping", ping)
 
-	//route handling with refactoring
-	router.GET("/external", controller.External)
-
-	//post routes
-	router.POST("/normal", postform)
-
-	router.Run("0.0.0.0:5000")
+	router.Run(":8000")
 }
