0ace6d01b7ca5b62d945d91fa595ee6c0892b245
rahulsai1999 <noneuser2183@gmail.com>
2020-03-31 12:44:23 +0530

    updated model

--- service/models/blog.go
+++ service/models/blog.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Blog -> model for Blog object
 type Blog struct {
-	ID     primitive.ObjectID `bson:"_id,omitempty`
-	Title  string
-	Author string
-	Body   string
+	ID     primitive.ObjectID `bson:"_id,omitempty"`
+	Title  string             `bson:"title,omitempty"`
+	Author string             `bson:"author,omitempty"`
+	Body   string             `bson:"body,omitempty"`
 }
